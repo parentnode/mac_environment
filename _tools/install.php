4d2248641390ab27bebeba7b2ca0b92d78e81b21
Martin Kæstel Nielsen <martin@think.dk>
2018-02-07 01:02:00 +0100

    Added explanation for mariaDB password

--- _tools/install.php
+++ _tools/install.php
@@ -269,7 +269,7 @@ $db_response = command("/opt/local/lib/mariadb/bin/mysql -u root mysql -e 'SHOW 
 // If not ACCESS DENIED, ask to set root password
 if(!preg_match("/^ERROR (1044|1045)/", $db_response)) {
 
-	$answer = ask("MySQL root password", array("[.]{8,30}"), true);
+	$answer = ask("Enter new root password for MariaDB (8-30 chars)", array("[.]{8,30}"), true);
 	command("sudo /opt/local/lib/mariadb/bin/mysqladmin -u root password '".$answer."'");
 }
 
