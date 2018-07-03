3206b7d60cc042c7a22cfebd13ed259a9df95f89
Martin Kæstel Nielsen <martin@think.dk>
2018-07-03 14:10:01 +0200

    Handle multiple servernames / doc roots

--- scripts/enable_site.sh
+++ scripts/enable_site.sh
@@ -34,96 +34,131 @@ if [ -e "$PWD/apache/httpd-vhosts.conf" ] ; then
 	if [ ! -z "$document_root" ] && [ ! -z "$server_name" ]; then
 
 		# Show collected data
-		echo "DocumentRoot:	$document_root"
+
+		# Possibly multible document roots in configuration
+		for line in $document_root; do 
+			echo "DocumentRoot: 	$line"; 
+		done
 		echo ""
-		echo "ServerName: 	$server_name"
+
+		# Possibly multible server names in configuration
+		for line in $server_name; do 
+			echo "ServerName: 	$line"; 
+		done
 
 		# ServerAlias not always present - only print if it is there
 		if [ ! -z "$server_alias" ]; then
-			echo "ServerAlias:	$server_alias"
+
+			# Possibly multible server alias' in configuration
+			for line in $server_alias; do 
+				echo "ServerAlias: 	$line"; 
+			done
 		fi
+
 
 		echo ""
 
 
 		# Updating apache.conf
 
-		# Get proper projects path (/srv/sites instead of /Users/username/Sites)
-		parentnode_project_path=$(echo "$document_root" | sed -e "s/\\/src\\/www//; s/\\/theme\\/www//")
+		# Possibly multible document roots in configuration
+		for line in $document_root; do 
 
-		# Don't enable sites which are already enabled
-		# Check if include path already exists in apache.conf
-		apache_entry_exists=$(grep -E "^Include [\"]?$parentnode_project_path\/apache\/httpd-vhosts.conf[\"]?" "$apache_file_path" || echo "")
-		if [ -z "$apache_entry_exists" ]; then
+			# Get proper projects path (/srv/sites instead of /Users/username/Sites)
+			parentnode_project_path=$(echo "$line" | sed -e "s/\\/src\\/www//; s/\\/theme\\/www//")
 
-			echo "Adding $parentnode_project_path/apache/httpd-vhosts.conf to apache.conf"
+			# Don't enable sites which are already enabled
+			# Check if include path already exists in apache.conf
+			apache_entry_exists=$(grep -E "^Include [\"]?$parentnode_project_path\/apache\/httpd-vhosts.conf[\"]?" "$apache_file_path" || echo "")
+			if [ -z "$apache_entry_exists" ]; then
 
-			# Include project cont in apache.conf
-			echo "Include \"$parentnode_project_path/apache/httpd-vhosts.conf\"" >> "$apache_file_path"
-			echo "" >> "$apache_file_path"
-		# project already exists in apache.conf
-		else
+				echo "Adding $parentnode_project_path/apache/httpd-vhosts.conf to apache.conf"
 
-			echo "Project already enabled in $apache_file_path"
+				# Include project cont in apache.conf
+				echo "" >> "$apache_file_path"
+				echo "Include \"$parentnode_project_path/apache/httpd-vhosts.conf\"" >> "$apache_file_path"
+			# project already exists in apache.conf
+			else
 
-		fi
+				echo "$line already enabled in $apache_file_path"
+
+			fi
+
+		done
+
+
+		echo ""
 
 
 		# Updating hosts
 
-		# Check hosts configuration
-		hosts_entry_exists=$(grep -E "[\t ]$server_name" "$host_file_path" || echo "")
 
-		if [ -z "$hosts_entry_exists" ]; then
+		# Make hosts file writable
+		sudo chmod 777 "$host_file_path"
 
-			echo ""
-			echo "Adding $server_name to $host_file_path"
+		# Possibly multible server names in configuration
+		for line in $server_name; do 
 
-			# Make hosts file writable
-			sudo chmod 777 "$host_file_path"
+			# Check hosts configuration
+			hosts_entry_exists=$(grep -E "$line" "$host_file_path" || echo "")
 
-			# Add hosts file entry
-			echo "" >> "$host_file_path"
-			echo "" >> "$host_file_path"
-			echo "127.0.0.1	$server_name" >> "$host_file_path"
-			echo "fe80::1%lo0	$server_name" >> "$host_file_path"
-			echo "::1			$server_name" >> "$host_file_path"
+			if [ -z "$hosts_entry_exists" ]; then
+
+				echo "Adding $line to $host_file_path"
 
 
-			# also add ServerAlias'
-			if [ ! -z "$server_alias" ]; then
+				# Add hosts file entry
+				echo "" >> "$host_file_path"
+				echo "" >> "$host_file_path"
+				echo "127.0.0.1	$line" >> "$host_file_path"
+				echo "fe80::1%lo0	$line" >> "$host_file_path"
+				echo "::1			$line" >> "$host_file_path"
 
-				echo ""
 
-				# Get current IP
-				ip=$(ifconfig en0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+			# Hosts entry already exists for current domain
+			else
 
-				# loop through server alias'
-				for domain in $(echo $server_alias | tr " " "\n")
-				do
-					echo "Adding $domain ($ip) to $host_file_path"
-					echo "$ip	$domain" >> "$host_file_path"
-				done
-				
+				echo "$line already enabled in $host_file_path"
+
 			fi
 
+		done
 
-			# Set correct hosts file permissions again
-			sudo chmod 644 "$host_file_path"
+		# also add ServerAlias'
+		if [ ! -z "$server_alias" ]; then
 
-			echo ""
+			# Get current IP
+			ip=$(ifconfig en0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
+			# Possibly multible server alias' in configuration
+			for line in $server_alias; do 
 
-		# Hosts entry already exists for current domain
-		else
+				# Check hosts configuration
+				hosts_entry_exists=$(grep -E "$line" "$host_file_path" || echo "")
 
-			echo "Project already enabled in $host_file_path"
+				if [ -z "$hosts_entry_exists" ]; then
 
+					# loop through server alias'
+					for domain in $(echo $line | tr " " "\n")
+					do
+						echo "Adding $domain ($ip) to $host_file_path"
+						echo "$ip	$domain" >> "$host_file_path"
+					done
+
+				fi
+
+			done
+		
 		fi
+
+
+		# Set correct hosts file permissions again
+		sudo chmod 644 "$host_file_path"
+
 
 		# Restart apache after modification
 		echo ""
-		echo "Restarting Apache"
+		echo "Restating Apache"
 		if [ -e "/opt/local/sbin/apachectl" ]; then
 			sudo /opt/local/sbin/apachectl restart
 		elif [ -e "/opt/local/apache2/apachectl" ]; then
@@ -152,4 +187,4 @@ else
 	echo "Apache configuration not found."
 	echo "You can only enable a site, if you run this command from the project root folder"
 
-fi;
+fi
