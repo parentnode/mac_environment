a89e7a4c3cf8093f57f2d227647a22ece66b2931
jessen1989 <jessen-it@outlook.com>
2019-02-21 16:19:44 +0100

    checking isInstalled function

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,37 +14,28 @@ source /srv/tools/scripts/functions.sh
 install_user=$(getCurrentUser)
 
 guiText "Installing system for $install_user" "Comment"
-xcode_array=("Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 10")
+xcode_array=("Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 28")
 guiText "Checking for xcode" "Comment"
 for item in "${xcode_array[@]}"
 do
     test_xcode=$(isInstalled "xcodebuild -version" "Xcode" "$item")
     export test_xcode
 done
-if [ -z "$test_xcode" ];
-then 
-    echo "Not Installed"
-else 
-    echo "$test_xcode"
-fi
-xcode_array_cl=("version: 6" "version: 7" "version: 8" "version: 9" "version: 10")
+testContent "$test_xcode"
+xcode_array_cl=("version: 6" "version: 7" "version: 8" "version: 9" "version: 28")
 guiText "Checking Xcode command line tools version" "Comment"
 for item in "${xcode_array_cl[@]}"
 do
     test_xcode_cl=$(isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "version:" "$item")
     export test_xcode_cl
 done
-if [ -z "$test_xcode_cl" ];
-then 
-    echo "Not Installed"
-else
-    echo "$test_xcode_cl"
-fi
+testContent "$test_xcode_cl"
+
 guiText "Checking for Macports" "Comment"
 test_macports=$(isInstalled "port version" "Version:" "Version: 2")
 export test_macports
-
 testContent "$test_macports"
+
 guiText "Test of read" "Comment"
 read -p "Do something: " something
 echo $something
