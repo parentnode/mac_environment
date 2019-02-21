5d58cdf93765e948397951ea994d52f10d8e2aba
jessen1989 <jessen-it@outlook.com>
2019-02-21 16:01:01 +0100

    checking isInstalled function

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,24 +18,26 @@ xcode_array=("Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 
 guiText "Checking for xcode" "Comment"
 for item in "${xcode_array[@]}"
 do
-    test=$(isInstalled "xcodebuild -version" "Xcode" "$item")
-    export test
+    test_xcode=$(isInstalled "xcodebuild -version" "Xcode" "$item")
+    export test_xcode
 done
-if [ -z $test];
+if [ -z "$test_xcode"];
 then 
     echo "Not Installed"
-else 
-    echo "Installed"
 fi
 xcode_array_cl=("version: 6" "version: 7" "version: 8" "version: 9" "version: 11")
 guiText "Checking Xcode command line tools version" "Comment"
 for item in "${xcode_array_cl[@]}"
 do
-    isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "version:" "$item"
+    test_xcode_cl=$(isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "version:" "$item")
+    export test_xcode_cl
 done
+if [ -z "$test_xcode_cl"];
+then 
+    echo "Not Installed"
+fi
 guiText "Checking for Macports" "Comment"
 isInstalled "port version" "Version:" "Version: 2"
-
 guiText "Test of read" "Comment"
 read -p "Do something: " something
 echo $something
