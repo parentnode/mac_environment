9a746031c979bcc7634603f2c88b728a93324d9b
jessen1989 <jessen-it@outlook.com>
2019-02-21 17:10:31 +0100

    checking isInstalled function

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,8 +39,7 @@ testContent "$test_macports" "Macports"
 guiText "Test of read" "Comment"
 read -p "Do something: " something
 echo $something
-cd /Users/$install_user/Desktop
-mkdir test
+mkdir -p /Users/$install_user/Desktop/test
 
 
 
