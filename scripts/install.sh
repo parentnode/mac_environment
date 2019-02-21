fca535675b6a51848871d806141ce565b7ee6be2
jessen1989 <jessen-it@outlook.com>
2019-02-21 12:50:05 +0100

    checking isInstalled function

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,9 +14,18 @@ source /srv/tools/scripts/functions.sh
 install_user=$(getCurrentUser)
 
 guiText "Installing system for $install_user" "Comment"
-array=("Xcode\ 4" "Xcode\ 5" "Xcode\ 6" "Xcode\ 7" "Xcode\ 8" "Xcode\ 9" "Xcode\ 10")
+xcode_array[0]="Xcode 4"
+xcode_array[1]="Xcode 5"
+xcode_array[2]="Xcode 6"
+xcode_array[3]="Xcode 7"
+xcode_array[4]="Xcode 8"
+xcode_array[5]="Xcode 9"
+xcode_array[6]="Xcode 10"
+
+    
+     #"Xcode\ 5" "Xcode\ 6" "Xcode\ 7" "Xcode\ 8" "Xcode\ 9" "Xcode\ 10")
 guiText "Checking for xcode" "Comment"
-for item in "${array[@]}"
+for item in "${xcode_array[@]}"
 do
     isInstalled "xcodebuild -version" "Xcode" $item
 done
