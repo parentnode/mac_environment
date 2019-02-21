dfb5f29bfae709dc8d1a69f160691bd342ca157b
jessen1989 <jessen-it@outlook.com>
2019-02-21 14:31:58 +0100

    checking isInstalled function

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,7 +21,7 @@ do
     isInstalled "xcodebuild -version" "Xcode" "$item"
 done
 guiText "Checking Xcode command line tools version" "Comment"
-xcode_array_cl=("version: 6" "version: 7" "version: 8" "version: 9" "version: 11")
+xcode_array_cl=("version: 6" "version: 7" "version: 8" "version: 9" "version: 10")
 for item in "${xcode_array_cl[@]}"
 do
     isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "version:" "$item"
