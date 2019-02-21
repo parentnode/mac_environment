64563943249a0618633cc1ad0838a9eec8fbbb35
jessen1989 <jessen-it@outlook.com>
2019-02-21 10:16:01 +0100

    new test for mac_environment

---
+++ scripts/functions.sh
@@ -0,0 +1,111 @@
+guiText(){
+	# Automatic comment format for simple setup as a text based gui
+	# eg. guiText "Redis" "Start"
+	# $1 Name of object to process
+	# $2 Type of process
+	case $2 in 
+		"Link")
+			echo
+			echo
+			echo "More info regarding $1"
+			echo "can be found on $3"
+			if [ -n "$4" ];
+			then
+				echo "and $4"
+			fi
+			echo
+			echo
+			;;
+		"Comment")
+			echo
+			echo "$1:"
+			if [ -n "$3" ];
+			then
+				echo "$3"
+			fi
+			echo
+			;;
+		"Section")
+			echo
+			echo 
+			echo "{---$1---}"	
+			echo
+			echo
+			;;
+		#These following commentary cases are used for installing and configuring setup
+		"Start")
+			echo
+			echo
+			echo "Starting installation process for $1"
+			echo
+			echo
+			;;
+		"Download")
+			echo
+			echo "Downloading files for the installation of $1"
+			echo "This could take some time depending on your internet connection"
+			echo "and hardware configuration"
+			echo
+			echo
+			;;
+		"Exist")
+			echo
+			echo "$1 allready exists"
+			if [ -n "$3" ];
+			then
+				echo "checking for $3"
+			fi
+			echo
+			echo
+			;;
+		"Install")
+			echo
+			echo "Configuring installation for $1"
+			if [ -n "$3" ]; then
+				echo "in $3"
+			fi
+			echo
+			;;
+		"Replace")
+			echo
+			echo "Replacing $1 with $3"
+			echo
+			;;
+		"Installed")
+			echo
+			echo "$1 Installed no need for more action at this point"
+			echo
+			;;
+		"Enable")
+			echo
+			echo "Enabling $1"
+			echo
+			;;
+		"Disable")
+			echo
+			echo "Disabling $2"
+			echo
+			;;
+		"Done")
+			echo
+			echo
+			echo "Installation process for $1 are done"
+			echo
+			echo
+			;;
+		"Skip")
+			echo
+			echo
+			echo "Skipping Installation process for $1"
+			echo
+			echo
+			;;
+		*)
+			echo 
+			echo "Are you sure you wanted to use gui text here?"
+			echo
+			;;
+
+	esac
+}
+export -f guiText
