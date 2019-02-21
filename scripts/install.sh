c940a61b716b9681a211b086f08756e1bdb3a7fc
jessen1989 <jessen-it@outlook.com>
2019-02-21 13:14:56 +0100

    checking isInstalled function

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,13 +14,18 @@ source /srv/tools/scripts/functions.sh
 install_user=$(getCurrentUser)
 
 guiText "Installing system for $install_user" "Comment"
-array=("Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 10")
+xcode_array=("Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 10")
 guiText "Checking for xcode" "Comment"
-for item in "${array[@]}"
+for item in "${xcode_array[@]}"
 do
     isInstalled "xcodebuild -version" "Xcode" "$item"
 done
-
+guiText "Checking Xcode command line tools version" "Comment"
+xcode_array_cl=("version: 6" "version: 7" "version: 8" "version: 9" "version: 10")
+for item in "${xcode_array_cl[@]}"
+do
+    isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "version"
+done
 read -p "Do something: " something
 echo $something
 
