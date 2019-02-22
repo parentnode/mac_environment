374bed828d6603c96b6e2f7f4ccfafafb5fcfe21
jessen1989 <jessen-it@outlook.com>
2019-02-22 09:40:37 +0100

    testing more generic functions

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -126,7 +126,7 @@ export -f getCurrentUser
 function isInstalled(){
 	check=$($1 | grep "^$2")
 	echo "$check"
-	if [ "$check" = "$3" ] && [ -n "$check" ] ;
+	if [ "$check" = *$3* ] && [ -n "$check" ] ;
 	then
 		echo "$3 installed"
 	fi
