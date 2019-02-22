9af1a2f79f046aff39427dbc0fb570a3085bc418
jessen1989 <jessen-it@outlook.com>
2019-02-22 09:34:28 +0100

    testing more generic functions

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ echo ""
 
 #
 source /srv/tools/scripts/functions.sh
-sudo ls >/dev/null
+#sudo ls >/dev/null
 install_user=$(getCurrentUser)
 
 guiText "Installing system for $install_user" "Comment"
