c5a48790bd2e36b5fb924e049da87809805c3fb0
jessen1989 <jessen-it@outlook.com>
2019-02-26 18:24:26 +0100

    testing new functions based on old ones

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -163,9 +163,9 @@ function isInstalled(){
 	#${xcode_array_cl[@]}
 	for item in "$2"
 	do
-		check=$(echo "$command" | grep "$item")
+		#check=$(echo "$command" | grep "$item")
 		# if anywhere in the output matches with one of the items in the array
-		if [ -n "$check" ]; then 
+		if [ "$command" = "$item" ]; then 
 			echo "true"
 		else 
 			echo "false"
