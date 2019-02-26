10d1a06d49a00a16a99126a4e5e46a884bf2a48a
jessen1989 <jessen-it@outlook.com>
2019-02-26 16:57:10 +0100

    testing new functions based on old ones

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -6,27 +6,27 @@ php_version="php72"
 
 guiText "Installing Software" "Section"
 
-guiText "Pointing Xcode towards the Developer directory instead of Xcode application bundle" "Comment"
+#guiText "Pointing Xcode towards the Developer directory instead of Xcode application bundle" "Comment"
 #sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer/
 
-guiText "Update macports" "Comment"
+#guiText "Update macports" "Comment"
 #sudo port selfupdate
 
-guiText "Enable getting PID of application really easy" "Comment"
+#guiText "Enable getting PID of application really easy" "Comment"
 #sudo port install pidof
 
-guiText "MariaDB" "Check"
-test_mariadb=$(isInstalled "port installed $mariadb_version-server" "mariadb" "mariadb-10")
+#guiText "MariaDB" "Check"
+#test_mariadb=$(isInstalled "port installed $mariadb_version-server" "mariadb" "mariadb-10")
 #sudo port -N install mariadb-10.2-server # Question: Are we going with 10.2 in mac ?
-testContent "$test_mariadb" "MariaDB" "macports" "$mariadb_version-server"
+#testContent "$test_mariadb" "MariaDB" "macports" "$mariadb_version-server"
 
-guiText "PHP" "Check"
-test_php=$(isInstalled "port installed $php_version" "php" "$php_version")
-testContent "$test_php" "PHP" "macports" "$php_version"
+#guiText "PHP" "Check"
+#test_php=$(isInstalled "port installed $php_version" "$php_version")
+#testContent "$test_php" "PHP" "macports" "$php_version"
 
-guiText "Apache" "Check"
-test_apache=$(isInstalled "port installed apache2" "apache" "apache2")
-testContent "$test_apache" "Apache" "macports" "apache2"
+#guiText "Apache" "Check"
+#test_apache=$(isInstalled "port installed apache2" "apache" "apache2")
+#testContent "$test_apache" "Apache" "macports" "apache2"
 
 
 
