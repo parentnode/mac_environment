9d38e1ba863b186a341ab33dbfc0b4ae65963e1b
jessen1989 <jessen-it@outlook.com>
2019-02-26 18:48:49 +0100

    testing new functions based on old ones

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -165,7 +165,7 @@ function isInstalled(){
 	#echo "$command"
 	for item in $2
 	do
-		echo "$item"
+		echo $item
 		#echo "Item: $item"
 		#check=$(echo "$command" | grep "$item")
 		#echo "check: $check"
