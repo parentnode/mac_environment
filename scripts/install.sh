333097ebb49e2d28dd778f0d9dc0ae1dbd809596
jessen1989 <jessen-it@outlook.com>
2019-02-26 11:18:45 +0100

    testing new functions based on old ones

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,11 +10,12 @@ echo ""
 
 #
 source /srv/tools/scripts/functions.sh
-#sudo ls >/dev/null
+
 install_user=$(getCurrentUser)
 
 guiText "Installing system for $install_user" "Comment"
 
+guiText "Checking for tools required for the installation process" "Section"
 # Array containing major releases of Xcode
 xcode_array=("Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 10")
 guiText "Checking for xcode" "Comment"
@@ -46,35 +47,35 @@ testContent "$test_macports" "Macports"
 #mkdir -p "/Users/$install_user/Desktop/$something"
 
 #output("\nChecking paths");
-#
-# check if configuration files are available
-#checkFile "myawesomefile.txt" "Required file is missing from your configuration source"
+
+guiText "Checking Required files/folders and shortcuts" "Section"
+
 checkFile "conf/httpd.conf" "Required file is missing from your configuration source"
 checkFile "conf/httpd-vhosts.conf" "Required file is missing from your configuration source"
 checkFile "conf/php.ini" "Required file is missing from your configuration source"
 checkFile "conf/my.cnf" "Required file is missing from your configuration source"
 checkFile "conf/apache.conf" "Required file is missing from your configuration source"
-#
-#
-#
+
 #// TODO: create .bash_profile if it does not exist
 #// Has not been tested
 checkFileOrCreate "~/.bash_profile" "conf/bash_profile.start"
-#
-#
-#
-#
+
 checkPath "~/Sites"
-#// set permissions
-#command("sudo chown $username:staff ~/Sites");
-#
-#checkPath("/srv");
-#// only create link if it doesn't exist already (making it twice makes a mess)
-#if(!file_exists("/srv/sites")) {
-#	command("sudo ln -s ~/Sites /srv/sites");
-#}
-#checkPath("~/Sites/apache");
-#checkPath("~/Sites/apache/logs");
+
+sudo chown $install_user:staff ~/Sites
+
+checkPath "/srv"
+if [ ! -f "/srv/sites" ]; then 
+    sudo ln -s ~/Sites /srv/sites
+fi
+
+checkPath "~/Sites/apache" 
+checkPath "~/Sites/apache/logs"
+
+# Software script
+bash /srv/tools/install_software.sh
+
+
 
 
 
