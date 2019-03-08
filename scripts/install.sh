efc569d1ddf365e4a64889e12c56517c8194322c
jesse1989 <jessen-it@outlook.com>
2019-03-08 12:52:37 +0100

    testing sh functions

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,15 +21,34 @@ guiText "Checking for tools required for the installation process" "Section"
 
 guiText "xcode" "Check"
 xcode_array=( "Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 10" )
-isInstalled "xcodebuild -version" "${xcode_array[@]}"
+$is_ok_xcode=isInstalled "xcodebuild -version" "${xcode_array[@]}"
+if [ "$is_ok_xcode" "Not Installed" ]; then
+    echo "$is_ok_xcode"
+    guiText "0" "Exit"
+else
+    echo "$is_ok_xcode"
+fi
 
 guiText "Xcode command line tools version" "Check"
 xcode_array_cl=( "version: 6" "version: 7" "version: 8" "version: 9" "version: 10" )
-isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "${xcode_array_cl[@]}"
+$is_ok_xcode_cl=isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "${xcode_array_cl[@]}" "xcode-select --install"
+if [ "$is_ok_xcode_cl" = "Not Installed" ]; then
+    echo "$is_ok_xcode_cl"
+    command "xcode-select --install"
+    guiText "0" "Exit"
+else
+    echo "$is_ok_xcode_cl"
+fi
 
 guiText "Macports" "Check"
 macports_array=("Version: 2")
-isInstalled "port version" "${macports_array[@]}"
+$is_ok_macports=isInstalled "port version" "${macports_array[@]}"
+if [ "$is_ok_macports" = "Not Installed" ]; then
+    echo "$is_ok_macports"
+    guiText "0" "Exit"
+else
+    echo "$is_ok_macports"
+fi
 
 #guiText "Test of read" "Comment"
 #read -p "So you want to father a folder give it a name: " something
@@ -38,12 +57,12 @@ isInstalled "port version" "${macports_array[@]}"
 #output("\nChecking paths");
 
 guiText "Checking Required files/folders and shortcuts" "Section"
-
-checkFile "conf/httpd.conf" "Required file is missing from your configuration source"
-checkFile "conf/httpd-vhosts.conf" "Required file is missing from your configuration source"
-checkFile "conf/php.ini" "Required file is missing from your configuration source"
-checkFile "conf/my.cnf" "Required file is missing from your configuration source"
-checkFile "conf/apache.conf" "Required file is missing from your configuration source"
+conf_path="/srv/tools/conf"
+checkFile "$conf_path/httpd.conf" "Required file is missing from your configuration source"
+checkFile "$conf_path/httpd-vhosts.conf" "Required file is missing from your configuration source"
+checkFile "$conf_path/php.ini" "Required file is missing from your configuration source"
+checkFile "$conf_path/my.cnf" "Required file is missing from your configuration source"
+checkFile "$conf_path/apache.conf" "Required file is missing from your configuration source"
 
 #// TODO: create .bash_profile if it does not exist
 #// Has not been tested
