d513d5c0b813d26bc404780484edb6860e0f0d6a
jessen1989 <jessen-it@outlook.com>
2019-03-26 14:57:35 +0100

    progress for mid-march on mac script

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,50 +11,90 @@ echo ""
 #
 source /srv/tools/scripts/functions.sh
 
+guiText "Gather information required for the installation" "Section"
+
 install_user=$(getCurrentUser)
 
 guiText "Installing system for $install_user" "Comment"
 
+valid_answers=("[Y n]")
+install_software=$(ask "Install software (Y/n)" "${valid_answers[@]}")
+export install_software
+
 guiText "Checking for tools required for the installation process" "Section"
+
+guiText "Checking for existing mariadb setup" "Section"
+# MYSQL ROOT PASSWORD
+
+#db_response=$(command "("/opt/local/lib/mariadb-10.2/bin/mysql -u root mysql -e 'SHOW TABLES'")" "false")
+#echo "$test_password"
+allowed_mariadb_server=("mariadb-10.2-server")
+mariadb_check=$(isInstalled "port installed" "${allowed_mariadb_server[@]}")
+
+if [ "$mariadb_check" = "Not Installed" ]; then
+    echo "$mariadb_check"
+    echo 
+    echo "Installer will continue and install mariadb"
+else
+    if [ -e "/srv/tools/scripts/password.txt" ];then
+	    sudo rm /srv/tools/scripts/password.txt
+    fi
+    root_password_status=$(/opt/local/lib/mariadb-10.2/bin/mysql -u root mysql -e 'SHOW TABLES' &> /srv/tools/scripts/password.txt)
+    test_password=$(grep "using password: NO" /srv/tools/scripts/password.txt || echo "")
+    if [ -z "$test_password" ];
+    then 
+        while [ "$test_password" ]
+        do
+            valid_password=("[A-Za-z0-9 \! \@ \— ]{8,30}")
+            maria_db_password=$(ask "Enter password" "${valid_password[@]}" "true" )
+            echo
+            verify_maria_db_password=$(ask "Verify password" "${valid_password[@]}" "true")
+            echo
+            if [ "$maria_db_password" != "$verify_maria_db_password" ]; then
+                echo "Password do not match"
+                echo "Try again"
+                echo
+            else
+                echo "Password match"
+                command "sudo /opt/local/lib/mariadb-10.2/bin/mysqladmin -u root password '"$maria_db_password"'" 
+            fi
+        done
+    else
+        echo "Password previously set"
+    fi
+    sudo rm /srv/tools/scripts/password.txt
+fi
+
+
+# SETTING DEFAULT GIT USER
+guiText "Setting Default GIT USER" "Section"
+git config --global core.filemode false
+#git config --global user.name "$install_user"
+#git config --global user.email "$install_email"
+
+# Checks if git credential are allready set, promts for input if not
+gitConfigured "name"
+gitConfigured "email"
+
+git config --global credential.helper cache
+command "sudo chown $install_user:staff /Users/$install_user/.gitconfig"
 
 # Array containing major releases of Xcode
 
 guiText "xcode" "Check"
 xcode_array=( "Xcode 4" "Xcode 5" "Xcode 6" "Xcode 7" "Xcode 8" "Xcode 9" "Xcode 10" )
-installOrNotToInstall "$( isInstalled "xcodebuild -version" "${xcode_array[@]}" )"
-#if [ "$is_ok_xcode" = "Not Installed" ]; then
-#    echo "$is_ok_xcode"
-#    guiText "0" "Exit"
-#else
-#    echo "$is_ok_xcode"
-#fi
+upgrade "$( isInstalled "xcodebuild -version" "${xcode_array[@]}" )"
+
 
 guiText "Xcode command line tools version" "Check"
 xcode_array_cl=( "version: 6" "version: 7" "version: 8" "version: 9" "version: 10" )
-installOrNotToInstall "$(isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "${xcode_array_cl[@]}")" "xcode-select --install"
-#if [ "$is_ok_xcode_cl" = "Not Installed" ]; then
-#    echo "$is_ok_xcode_cl"
-#    command "xcode-select --install"
-#    guiText "0" "Exit"
-#else
-#    echo "$is_ok_xcode_cl"
-#fi
+upgrade "$(isInstalled "pkgutil --pkg-info=com.apple.pkg.CLTools_Executables" "${xcode_array_cl[@]}")" "" "xcode-select --install"
+
 
 guiText "Macports" "Check"
 macports_array=("Version: 2")
-installOrNotToInstall "$(isInstalled "port version" "${macports_array[@]}")"
-#if [ "$is_ok_macports" = "Not Installed" ]; then
-#    echo "$is_ok_macports"
-#    guiText "0" "Exit"
-#else
-#    echo "$is_ok_macports"
-#fi
+upgrade "$(isInstalled "port version" "${macports_array[@]}")"
 
-#guiText "Test of read" "Comment"
-#read -p "So you want to father a folder give it a name: " something
-#mkdir -p "/Users/$install_user/Desktop/$something"
-
-#output("\nChecking paths");
 
 guiText "Checking Required files/folders and shortcuts" "Section"
 conf_path="/srv/tools/conf"
@@ -66,27 +106,96 @@ checkFile "$conf_path/apache.conf" "Required file is missing from your configura
 
 #// TODO: create .bash_profile if it does not exist
 #// Has not been tested
-checkFileOrCreate "~/.bash_profile" "/srv/tools/conf/bash_profile.start"
+checkFileOrCreate "/Users/$install_user/.bash_profile" "/srv/tools/conf/bash_profile.start"
 
-checkPath "~/Sites"
+checkPath "/Users/$install_user/Sites"
 #
 sudo chown $install_user:staff ~/Sites
 
 checkPath "/srv"
-if [ -f "/srv/sites" ]; then 
+if [ -d "/srv/sites" ]; then 
     echo "/srv/sites exists"
 else
     echo "Creating symlink"
-    sudo ln -s ~/Sites /srv/sites
+    sudo ln -s /Users/$install_user/Sites /srv/sites
 fi
 
-checkPath "~/Sites/apache" 
-checkPath "~/Sites/apache/logs"
+checkPath "/Users/$install_user/Sites/apache" 
+checkPath "/Users/$install_user/Sites/apache/logs"
 
-# Software script
-bash /srv/tools/scripts/install_software.sh
+## Software script
+#if test "$install_software" = "Y"; then
+#    bash /srv/tools/scripts/install_software.sh
+#else
+#    guiText "Skipping software installation" "Comment"
+#fi
+
+#test placeholder replacing
+command "sudo chown $install_user:staff ~/Sites"
+
+#mysql paths
+checkPath "/opt/local/var/run/mariadb-10.2"
+checkPath "/opt/local/var/db/mariadb-10.2"
+checkPath "/opt/local/etc/mariadb-10.2"
+checkPath "/opt/local/share/mariadb-10.2"
 
 
+#Mysql preparations
+command "sudo chown -R mysql:mysql /opt/local/var/db/mariadb-10.2"
+command "sudo chown -R mysql:mysql /opt/local/var/run/mariadb-10.2"
+command "sudo chown -R mysql:mysql /opt/local/etc/mariadb-10.2"
+command "sudo chown -R mysql:mysql /opt/local/share/mariadb-10.2"
+
+
+#// set permissions
+command "sudo chown $install_user:staff ~/.gitconfig"
+
+
+copyFile "/srv/tools/conf/my.cnf", "/opt/local/etc/mariadb-10.2/my.cnf" 
+
+copyFile "/srv/tools/conf/httpd.conf" "/opt/local/etc/apache2/httpd.conf"
+
+command "sudo chmod 777 /opt/local/etc/apache2/httpd.conf"
+
+#// update username in file to make apache run as current user (required to access vhosts in dropbox)
+replaceInFile "/opt/local/etc/apache2/httpd.conf" "###USERNAME###" $install_user
+
+command "sudo chmod 644 /opt/local/etc/apache2/httpd.conf"
+
+copyFile "/srv/tools/conf/httpd-vhosts.conf" "/opt/local/etc/apache2/extra/httpd-vhosts.conf"
+
+copyFile "/srv/tools/conf/httpd-ssl.conf" "/opt/local/etc/apache2/extra/httpd-ssl.conf"
+
+if [ ! -e "/srv/sites/apache/apache.conf" ]; then
+    copyFile "/srv/tools/conf/apache.conf" "/srv/sites/apache/apache.conf"
+    command "sudo chown -R $install_user:staff /Users/$install_user/Sites/apache"
+fi     
+
+copyFile "/srv/tools/conf/newsyslog-apache.conf" "/etc/newsyslog.d/apache.conf"
+
+copyFile "/srv/tools/conf/php.ini" "/opt/local/etc/php72/php.ini"
+#// copy php.ini.default for native configuration
+#copyFile("conf/php_ini_native.ini", "/etc/php.ini", "sudo");
+copyFile "/srv/tools/conf/php_ini_native.ini" "/etc/php.ini"
+
+
+#output("\nConfiguration copied");
+
+#// Add alias' to .bash_profile
+#checkFileContent("~/.bash_profile", "conf/bash_profile.default");
+#// set correct owner for .bash_profile
+#command("sudo chown $username:staff ~/.bash_profile");
+
+
+#// Add local domains to /etc/hosts
+#command("sudo chmod 777 /etc/hosts");
+#checkFileContent("/etc/hosts", "conf/hosts.default");
+#command("sudo chmod 644 /etc/hosts");
+
+
+#// start database
+#command("sudo /opt/local/share/mariadb-10.2/support-files/mysql.server start",true);
+#output("Starting MariaDB");
 
 #email_array=("[A-Za-z0-9\.\-]+@[A-Za-z0-9\.\-]+\.[a-z]{2,10}")
 #ask "Email" "${email_array[@]}"
--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -1,40 +1,51 @@
 #!/bin/bash -e
 
-mariadb_version="mariadb-10"
-php_version="php72"
-
-
 guiText "Installing Software" "Section"
 
 guiText "Pointing Xcode towards the Developer directory instead of Xcode application bundle" "Comment"
-#command "sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer/"
+command "sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer/"
 
 guiText "Update macports" "Comment"
-#command "sudo port selfupdate"
+command "sudo port selfupdate"
 
 guiText "Enable getting PID of application really easy" "Comment"
-#command "sudo port install pidof"
+command "sudo port install pidof"
 
-guiText "MariaDB" "Check"
-mariadb_array=("mariadb-10.1" "mariadb-10.2" "mariadb-10.3")
-#isInstalled "port installed" "${mariadb_array[@]}"
-#is_ok_mariadb=$( isInstalled "port installed" "${mariadb_array[@]}" )
-installOrNotToInstall "$(isInstalled "port installed" "${mariadb_array[@]}")" "sudo port -N install mariadb-10.2-server"
-#sudo port -N install mariadb-10.2-server # Question: Are we going with 10.2 in mac ?
-##testContent "$test_mariadb" "MariaDB" "macports" "$mariadb_version-server"
-
-#guiText "PHP" "Check"
-initial_install_array=("php72" "apache2" "mariadb-10.2-server" "pear" "php72-apache2handler")
-installOrNotToInstall "$(isInstalled "port installed" "${initial_install_array[@]}")" "sudo port -N install php72 +apache2 +mariadb-10.2-server +pear php72-apache2handler"
-#command("sudo port -N install php72 +apache2 +mariadb-server +pear php72-apache2handler");
-
-#test_php=$(isInstalled "port installed $php_version" "$php_version")
-#testContent "$test_php" "PHP" "macports" "$php_version"
-
-guiText "Apache" "Check"
-#test_apache=$(isInstalled "port installed apache2" "apache" "apache2")
-#testContent "$test_apache" "Apache" "macports" "apache2"
-
+guiText "Installing MariaDB" "Comment"
+command "sudo port -N install mariadb-10.2-server"
+initial_install_array=("php72" "apache2" "mariadb-server" "pear" "php72-apache2handler")
+for ((i=0; i < ${#initial_install_array[@]}; i++))
+do 
+    command "sudo port -N install ${initial_install_array[$i]}"
+done
+guiText "Installing PHP72-mysql" "Comment"
+command "sudo port -N install php72-mysql"
+guiText "Installing PHP72-openssl" "Comment"
+command "sudo port -N install php72-openssl"
+guiText "Installing PHP72-mbstring" "Comment"
+command "sudo port -N install php72-mbstring"
+guiText "Installing PHP72-curl" "Comment"
+command "sudo port -N install php72-curl"
+guiText "Installing PHP72-zip" "Comment"
+command "sudo port -N install php72-zip"
+guiText "Installing PHP72-imagick" "Comment"
+command "sudo port -N install php72-imagick"
+guiText "Installing PHP72-igbinary" "Comment"
+command "sudo port -N install php72-igbinary"
+#guiText "Installing PHP72-memcached" "Comment"
+##command "sudo port install php72-memcached"
+guiText "Installing PHP72-redis" "Comment"
+command "sudo port -N install php72-redis"
+guiText "Set PHP php72" "Comment" 
+command "sudo port select --set php php72"
+guiText "Loading redis" "Comment"
+command "sudo port load redis"
+guiText "Installing ffmpeg" "Comment"
+command "sudo port -N install ffmpeg +nonfree"
+#autostart apache on boot
+guiText "Load apache2" "Comment"
+command "sudo port load apache2"
+guiText "Software Installed" "Comment"
 
 
 #guiText "Installing "
