d01a81e9b88366614a575eaa1a5ead8807a0653c
jessen1989 <jessen-it@outlook.com>
2019-06-11 13:12:47 +0200

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ guiText "Gather information required for the installation" "Section"
 install_user=$(getCurrentUser)
 export install_user
 if [ "$install_user" = "root" ]; then
-    echo "User cannot be root!: check previous steps and try running the installer again"
+    exit "User cannot be root!: check previous steps and try running the installer again"
 else
     guiText "Installing system for $install_user" "Comment"
 
