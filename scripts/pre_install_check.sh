f3a20e88c9a0ca60daafe071193af5f041410927
jessen1989 <jessen-it@outlook.com>
2019-06-12 12:46:19 +0200

    simplified functions

--- scripts/pre_install_check.sh
+++ scripts/pre_install_check.sh
@@ -13,7 +13,7 @@ if [ "$mariadb_check" = "Not Installed" ]; then
     echo 
     echo "Installer will continue and install mariadb"
 else
-    root_password_status=$(/opt/local/lib/mariadb-10.2/bin/mysql -u root mysql -e 'SHOW TABLES' | grep "using password: NO")
+    root_password_status=$(/opt/local/lib/mariadb-10.2/bin/mysql -u root mysql -e 'SHOW TABLES' | grep "using password: NO" || echo "")
     #root_password_status=$(/opt/local/lib/mariadb-10.2/bin/mysql -u root mysql -e 'SHOW TABLES' &> $test)
     #test_password=$(echo "$root_password_status" | grep "using password: NO" || echo "")
     echo "$root_password_status"
