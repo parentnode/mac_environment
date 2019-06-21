3ae5f13b0a28ead7cb9023bb209f5d3ff49771f8
jessen1989 <jessen-it@outlook.com>
2019-06-21 12:23:08 +0200

    added output Handler

---
+++ tests/output_handler.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+source /srv/sites/parentnode/mac_environment/scripts/functions.sh
+
+outputHandler "Section" "outputHandler test"
+outputHandler "Section"
+
+outputHandler "Comment" "This i a comment all about how this message got twisted upside down I like to take a minute just sit'in right here." "And tell you how this became the best text output out there"
+outputHandler "Comment" "Nothing special to comment on"
+outputHandler "Comment"
+
+outputHandler "No no  no" "Test"
+
+outputHandler "Exit"
+outputHandler "Exit" "I'm leaving for ever"
