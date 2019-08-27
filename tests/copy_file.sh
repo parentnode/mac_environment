f8ab319e8f61e28aa1d23be497fa6b1c30f6a76b
jessen1989 <jessen-it@outlook.com>
2019-08-27 11:50:10 +0200

    testing copy_file

--- tests/copy_file.sh
+++ tests/copy_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/mac_environment/scripts/functions.sh
 
-copyFile "/Users/$(getUsername)/Desktop/ouput.sh" "/Users/$(getUsername)/Desktop/copied_output.sh"
+copyFile "/Users/$(getUsername)/Desktop/ouputs.sh" "/Users/$(getUsername)/Desktop/copied_output.sh"
