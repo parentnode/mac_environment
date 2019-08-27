02c299afa93e83ba84f5ad30680e3ead27c3ca8b
jessen1989 <jessen-it@outlook.com>
2019-08-27 12:02:44 +0200

    testing copy_file

--- tests/copy_file.sh
+++ tests/copy_file.sh
@@ -3,5 +3,5 @@ source /srv/sites/parentnode/mac_environment/scripts/functions.sh
 
 #copyFile "/Users/$(getUsername)/Desktop/ouputs.sh" "/Users/$(getUsername)/Desktop/copied_output.sh"
 if [ -e "/Users/$(getUsername)/Desktop/ouputs.sh" ]; then
-    echo "im real and im here tadaa"
+    echo "im real and im here "
 fi
