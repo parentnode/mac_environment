0bb1ca372bcca458544c30c9cd4db25c83d7c1db
jessen1989 <jessen-it@outlook.com>
2019-08-27 12:05:57 +0200

    testing copy_file

--- tests/copy_file.sh
+++ tests/copy_file.sh
@@ -2,6 +2,6 @@
 source /srv/sites/parentnode/mac_environment/scripts/functions.sh
 install_user=$(getUsername)
 #copyFile "/Users/$(getUsername)/Desktop/ouputs.sh" "/Users/$(getUsername)/Desktop/copied_output.sh"
-if [ -e "/Users/$install_user/Desktop/ouputs.sh" ]; then
+if [ -e "/Users/$install_user/.bash_profile" ]; then
     echo "im real and im here now"
 fi
