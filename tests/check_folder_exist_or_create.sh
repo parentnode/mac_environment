1e52806fc67e0dab80eec392a9fd1c37798a0c3c
jessen1989 <jessen-it@outlook.com>
2019-09-12 13:27:17 +0200

    added checkFolderExistOrCreate test

---
+++ tests/check_folder_exist_or_create.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+source /srv/sites/parentnode/mac_environment/scripts/functions.sh
+
+checkFolderExistOrCreate "/Users/davidjessen/Sites/parentnode/mac_environment/tests/created_folder"
