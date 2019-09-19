b6c47edebf4e9ee21102276094a6432721d78952
jessen1989 <jessen-it@outlook.com>
2019-09-19 16:47:54 +0200

    test pre check

--- scripts/pre_install_check.sh
+++ scripts/pre_install_check.sh
@@ -124,13 +124,13 @@ else
 fi
 
 
-#outputHandler "comment" "Checking for Macports"
-#macports_array=("Version: 2")
+outputHandler "comment" "Checking for Macports"
+macports_array=("Version: 2")
 
 #upgrade "$(isInstalled "port version" "${macports_array[@]}")"
-#if [ $(testCommand "port version" "${macports_array[@]}") = "true" ]; then
-#    outputHandler "comment" "Macports installed"
-#else
-#    outputHandler "exit" "Update macports and try again"
-#fi
+if [ $(testCommand "port version" "${macports_array[@]}") = "true" ]; then
+    outputHandler "comment" "Macports installed"
+else
+    outputHandler "exit" "Update macports and try again"
+fi
 
