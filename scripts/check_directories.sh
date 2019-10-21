f3ae8a9b5b387bdb9f2dc02fee8e99686a7d49ad
jessen1989 <jessen-it@outlook.com>
2019-10-21 14:14:51 +0200

    back to basic testing \~ in mac

--- scripts/check_directories.sh
+++ scripts/check_directories.sh
@@ -44,10 +44,10 @@ else
     echo "Creating symlink"
     sudo ln -s /Users/$install_user/Sites /srv/sites
 fi
-if [ -e "~/Sites/apache" ]; then
+if [ -d "/Users/$install_user/Sites/apache" ]; then
     echo "found Sites Apache "
 fi
-if [ -e "~/Sites/apache/logs" ]; then
+if [ -d "/Users/$install_user/Sites/apache/logs" ]; then
     echo "found Sites Apache logs "
 fi
 #checkFolderExistOrCreate "/Users/$install_user/Sites/apache" 
