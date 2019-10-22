d343589f8561f47581b6e862e87a7e4a6b9a2f5c
jessen1989 <jessen-it@outlook.com>
2019-10-22 15:08:16 +0200

    avoiding redis for the moment

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -54,7 +54,7 @@ if [ "$install_software" = "Y" ]; then
     outputHandler "comment" "Software Installed"
 
     #test placeholder replacing
-    command "sudo chown $install_user:staff ~/Sites"
+    command "sudo chown $install_user:staff /User/$install_user/Sites"
 
     #mysql paths
     checkFolderExistOrCreate "/opt/local/var/run/mariadb"
