619db4254eefa92582ec64a76f9da3426774da1e
jessen1989 <jessen-it@outlook.com>
2019-10-29 11:48:32 +0100

    Added comments for script

--- scripts/check_directories.sh
+++ scripts/check_directories.sh
@@ -1,5 +1,5 @@
 outputHandler "section" "Checking Required files/folders and shortcuts"
-
+# Just a shorthand for readability
 conf_path="/srv/tools/conf"
 if [ $(fileExist "$conf_path/httpd.conf") = "false" ]; then
     echo "Required file is missing from your configuration source"
@@ -16,22 +16,16 @@ fi
 if [ $(fileExist "$conf_path/apache.conf") = "false" ]; then
     echo "Required file is missing from your configuration source"
 fi
-#checkFile "$conf_path/httpd-vhosts.conf" "Required file is missing from your configuration source"
-#checkFile "$conf_path/php.ini" "Required file is missing from your configuration source"
-#checkFile "$conf_path/my.cnf" "Required file is missing from your configuration source"
-#checkFile "$conf_path/apache.conf" "Required file is missing from your configuration source"
 
-#// TODO: create .bash_profile if it does not exist
-#// Has not been tested
 if [ $(fileExist "/Users/$install_user/.bash_profile") = "false" ]; then
     echo "Creating .bash_profile"
     copyFile "/srv/tools/conf/bash_profile_full.default" "/Users/$install_user/.bash_profile"
 else 
     echo "Existing .bash_profile"
 fi
-#checkFileOrCreate "/Users/$install_user/.bash_profile" "/srv/tools/conf/bash_profile.start"
-checkFolderExistOrCreate "/Users/$install_user/Sites"
 
+checkFolderExistOrCreate "/Users/$install_user/Sites"
+# Change localuser group of /Users/$install_user/Sites to staff 
 sudo chown $install_user:staff /Users/$install_user/Sites
 
 checkFolderExistOrCreate "/srv"
