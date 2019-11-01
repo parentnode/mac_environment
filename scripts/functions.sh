14c34c71c2b0c51221485785386810f13319b6a4
jessen1989 <jessen-it@outlook.com>
2019-11-01 14:42:45 +0100

    added bugfixes

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -126,10 +126,10 @@ checkGitCredential(){
 export -f checkGitCredential
 
 checkMariadbPassword(){
-	mariadb_installed_array=("(active)")
-	mariadb_installed=$(testCommand "port installed mariadb-server" "$mariadb_installed_array")
+	mariadb_installed_array=("@10.[0-9].* (active)")
+	#mariadb_installed=$(testCommand "port installed mariadb-server" "$mariadb_installed_array")
 	mariadb_installed_specific=$(testCommand "port installed mariadb-*-server" "$mariadb_installed_array")
-	if [ "$mariadb_installed" = "true" ] || [ "$mariadb_installed_specific" = "true" ]; then
+	if [ "$mariadb_installed_specific" = "true" ]; then
 		mariadb_status_array=("mysql")
 		mariadb_status=$(testCommand "ps -Aclw" "${mariadb_status_array[@]}")
 		if [ "$mariadb_status" = "true" ]; then 
