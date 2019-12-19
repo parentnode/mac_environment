1279053f00f2363b97a37181a356121ff761f29f
jessen1989 <jessen-it@outlook.com>
2019-12-19 16:54:43 +0100

    mac catalina fix

--- scripts/check_directories.sh
+++ scripts/check_directories.sh
@@ -25,6 +25,7 @@ else
 fi
 
 #checkFolderExistOrCreate "/Users/$install_user/Sites"
+checkFolderExistOrCreate "/Users/$install_user/Sites"
 
 #checkFolderExistOrCreate "/srv"
 outputHandler "comment" "Sites folder setup"
@@ -32,8 +33,7 @@ catalina='10.15.[0-9]'
 macos_version=$(sw_vers | grep -E "ProductVersion:" | cut -f2)
 macos_version_catalina=$(sw_vers | grep -E "ProductVersion:\t$catalina" | cut -f2)
 if [ "$macos_version" = "$macos_version_catalina" ]; then
-    if [ ! -d ~/Sites ]; then 
-        checkFolderExistOrCreate "/Users/$install_user/Sites"
+    if [ ! -d /srv/sites ]; then 
         sudo ln -s ~/Sites /var/parentnode/sites
     fi
 else
