a1c0e9fc81cb16eaa0a5c7d8d7f036f5c1762299
jessen1989 <jessen-it@outlook.com>
2020-01-06 11:43:32 +0100

    Adding changes to bash profile and check directories symlink

--- scripts/check_directories.sh
+++ scripts/check_directories.sh
@@ -26,28 +26,18 @@ fi
 
 #checkFolderExistOrCreate "/Users/$install_user/Sites"
 checkFolderExistOrCreate "/Users/$install_user/Sites"
+sudo chown $install_user:staff ~/Sites
 
 #checkFolderExistOrCreate "/srv"
 outputHandler "comment" "Sites folder setup"
-catalina='10.15.[0-9]'  
-macos_version=$(sw_vers | grep -E "ProductVersion:" | cut -f2)
-macos_version_catalina=$(sw_vers | grep -E "ProductVersion:\t$catalina" | cut -f2)
-if [ "$macos_version" = "$macos_version_catalina" ]; then
-    echo "Catalina"
-    if [ ! -d /srv/sites ]; then 
-        sudo ln -s ~/Sites /var/parentnode/sites
-    fi
-else
-    checkFolderExistOrCreate "/srv"
-    if [ ! -d "/srv/sites" ]; then
+if [ ! -d ~/Sites  ]; then
+    mkdir ~/Sites
+fi
+if [ ! -d /srv/sites ]; then 
         sudo ln -s ~/Sites /srv/sites
-    else
-        echo "not needed for your setup"
-    fi
 fi
 
 # Change localuser group of /Users/$install_user/Sites to staff 
-sudo chown $install_user:staff /Users/$install_user/Sites
 
 checkFolderExistOrCreate "/Users/$install_user/Sites/apache" 
 checkFolderExistOrCreate "/Users/$install_user/Sites/apache/logs"
