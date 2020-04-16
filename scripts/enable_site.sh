148d9be1c6dd34bb42c82f6623abdbcdb5079cb1
jessen1989 <jessen-it@outlook.com>
2020-04-16 14:50:31 +0200

    testing new enable site mac

--- scripts/enable_site.sh
+++ scripts/enable_site.sh
@@ -41,6 +41,18 @@ setHost(){
 	fi
 	sudo chmod 644 "$host_file_path"
 }
+enablingApacheSite(){
+	include=$(echo "Include \"$(getSiteInfo "$1" | sed s,/theme/www,/apache/httpd-vhosts.conf, )\"")
+	apache_entry_exists=$(grep "$include" "$apache_file_path" || echo "")
+	#echo "$include"
+	#echo "Apache Entry: $apache_entry_exists"
+	if [ -z "$apache_entry_exists" ]; then
+		echo "enabling conf in $apache_file_path"
+		echo "$include" >> "$apache_file_path"
+	else
+		echo "Virtual Host allready enabled in $apache_file_path"
+	fi
+}
 
 # Parse DocumentRoot from httpd-vhosts.conf
 document_root=($(grep -E "DocumentRoot" "$PWD/apache/httpd-vhosts.conf" | sed -e "s/	DocumentRoot \"//; s/\"//"))
@@ -53,19 +65,131 @@ export server_name
 # Parse ServerAlias from httpd-vhosts.conf
 server_alias=($(grep -E "ServerAlias" "$PWD/apache/httpd-vhosts.conf" | sed "s/	ServerAlias //"))
 export server_alias
-for alias in $(getSiteInfo "${server_alias[@]}")
-do
-	echo "$alias"
-done
-for server in $(getSiteInfo "${server_name[@]}")
-do
-	setHost "$server"
-done
-for doc in $(getSiteInfo "${document_root[@]}")
-do
-	echo "$doc"
-done
+#for alias in $(getSiteInfo "${server_alias[@]}")
+#do
+#	echo "$alias"
+#done
+#for server in $(getSiteInfo "${server_name[@]}")
+#do
+#	setHost "$server"
+#done
+#for doc in $(getSiteInfo "${document_root[@]}")
+#do
+#	echo "$doc"
+#done
+# Does current location seem to fullfil requirements (is httpd-vhosts.conf found where it is expected to be found)
+if [ -e "$PWD/apache/httpd-vhosts.conf" ] ; then
 
+	# Parse DocumentRoot from httpd-vhosts.conf
+	document_root=($(grep -E "DocumentRoot" "$PWD/apache/httpd-vhosts.conf" | sed -e "s/	DocumentRoot \"//; s/\"//"))
+	export document_root
+	
+	# Parse ServerName from httpd-vhosts.conf
+	server_name=($(grep -E "ServerName" "$PWD/apache/httpd-vhosts.conf" | sed "s/	ServerName //"))
+	export server_name
+	
+	# Parse ServerAlias from httpd-vhosts.conf
+	server_alias=($(grep -E "ServerAlias" "$PWD/apache/httpd-vhosts.conf" | sed "s/	ServerAlias //"))
+    export server_alias
+
+	# Could not find DocumentRoot or ServerName
+    if [ -z "$(getSiteInfo "${document_root[@]}")" ] && [ -z "$(getSiteInfo "${server_name[@]}")" ]; then
+		echo ""
+		echo "Apache configuration seems to be broken."
+		echo "Please revert any changes you have made to the https-vhosts.conf file."
+		echo ""
+	else
+		echo "Setting up site"
+		
+		for alias in $(getSiteInfo "${server_alias[@]}")
+		do
+			echo "$alias"
+		done
+		# Updating apache.conf
+
+		for doc in $(getSiteInfo "${document_root[@]}")
+		do
+			enablingApacheSite "$doc"
+		done
+	fi
+
+	# Updating hosts
+	for server in $(getSiteInfo "${server_name[@]}")
+	do
+		setHost "$server"
+	done
+	
+	# Restart a# Does current location seem to fullfil requirements (is httpd-vhosts.conf found where it is expected to be found)
+if [ -e "$PWD/apache/httpd-vhosts.conf" ] ; then
+
+	# Parse DocumentRoot from httpd-vhosts.conf
+	document_root=($(grep -E "DocumentRoot" "$PWD/apache/httpd-vhosts.conf" | sed -e "s/	DocumentRoot \"//; s/\"//"))
+	export document_root
+	
+	# Parse ServerName from httpd-vhosts.conf
+	server_name=($(grep -E "ServerName" "$PWD/apache/httpd-vhosts.conf" | sed "s/	ServerName //"))
+	export server_name
+	
+	# Parse ServerAlias from httpd-vhosts.conf
+	server_alias=($(grep -E "ServerAlias" "$PWD/apache/httpd-vhosts.conf" | sed "s/	ServerAlias //"))
+    export server_alias
+
+	# Could not find DocumentRoot or ServerName
+    if [ -z "$(getSiteInfo "${document_root[@]}")" ] && [ -z "$(getSiteInfo "${server_name[@]}")" ]; then
+		echo ""
+		echo "Apache configuration seems to be broken."
+		echo "Please revert any changes you have made to the https-vhosts.conf file."
+		echo ""
+	else
+		echo "Setting up site"
+		
+		for alias in $(getSiteInfo "${server_alias[@]}")
+		do
+			echo "$alias"
+		done
+		# Updating apache.conf
+
+		for doc in $(getSiteInfo "${document_root[@]}")
+		do
+			enablingApacheSite "$doc"
+		done
+	fi
+
+	# Updating hosts
+	for server in $(getSiteInfo "${server_name[@]}")
+	do
+		setHost "$server"
+	done
+	
+	# Restart apache after modification
+	echo ""
+	echo "Restarting Apache"
+	sudo service apache2 restart
+
+
+	echo ""
+	echo "Site enabled: OK"
+	echo ""
+else
+
+	echo "Apache configuration not found."
+	echo "You can only enable a site, if you run this command from the project root folder"
+
+fipache after modification
+	echo ""
+	echo "Restarting Apache"
+	sudo service apache2 restart
+
+
+	echo ""
+	echo "Site enabled: OK"
+	echo ""
+else
+
+	echo "Apache configuration not found."
+	echo "You can only enable a site, if you run this command from the project root folder"
+
+fi
 
 ## Does current location seem to fullfil requirements (is httpd-vhosts.conf found where it is expected to be found)
 #if [ -e "$PWD/apache/httpd-vhosts.conf" ] ; then
