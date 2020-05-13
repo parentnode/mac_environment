b19f0a8150e5767d5da84d75135d7f2a8b4d262b
jessen1989 <jessen-it@outlook.com>
2020-05-13 19:59:15 +0200

    keeping tests relevant for mac script checkFolderExistOrCreate

--- tests/check_folder_exist_or_create.sh
+++ tests/check_folder_exist_or_create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/mac_environment/scripts/functions.sh
 
-checkFolderExistOrCreate "/Users/davidjessen/Sites/parentnode/mac_environment/tests/created_folder"
+checkFolderExistOrCreate "/Users/$(echo $(logname))/Sites/parentnode/mac_environment/tests/created_folder"
