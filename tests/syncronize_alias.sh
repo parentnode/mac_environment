ff99525045449f90dd890ce0f674d563e34ed4e3
jessen1989 <jessen-it@outlook.com>
2020-05-13 19:17:22 +0200

    keeping tests relevant for mac script syncronize alias

--- tests/syncronize_alias.sh
+++ tests/syncronize_alias.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/mac_environment/scripts/functions.sh
 
-syncronizeAlias
+syncronizeAlias "alias" "/srv/tools/conf/dot_profile_alias" "$HOME/.bash_profile"
