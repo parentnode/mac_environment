18cf9ab500f7ba6f63e8065706a3947a265f4033
jessen1989 <jessen-it@outlook.com>
2020-05-13 19:31:19 +0200

    keeping tests relevant for mac script deleteAndAppendSection

---
+++ tests/delete_and_append_section.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+source /srv/sites/parentnode/mac_environment/scripts/functions.sh
+deleteAndAppendSection "#section" "/Users/davidjessen/Sites/parentnode/mac_environment/tests/update_content_test_files/source" "/Users/davidjessen/Sites/parentnode/mac_environment/tests/update_content_test_files/destination"
