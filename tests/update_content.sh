87ffee1edeff0b46432f0615f23a791a9841d2b8
jessen1989 <jessen-it@outlook.com>
2020-05-13 19:31:19 +0200

    keeping tests relevant for mac script deleteAndAppendSection

--- tests/update_content.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-source /srv/sites/parentnode/mac_environment/scripts/functions.sh
-deleteAndAppendSection "# enable_git_prompt" "/Users/davidjessen/Sites/parentnode/mac_environment/tests/update_content_test_files/source" "/Users/davidjessen/Sites/parentnode/mac_environment/tests/update_content_test_files/destination"
-deleteAndAppendSection "# alias" "/Users/davidjessen/Sites/parentnode/mac_environment/tests/update_content_test_files/source" "/Users/davidjessen/Sites/parentnode/mac_environment/tests/update_content_test_files/destination"
