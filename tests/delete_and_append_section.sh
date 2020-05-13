1b8070b58b5b237ee72a0c97d6973cbfde9c900e
jessen1989 <jessen-it@outlook.com>
2020-05-13 19:49:31 +0200

    keeping tests relevant for mac script deleteAndAppendSection

--- tests/delete_and_append_section.sh
+++ tests/delete_and_append_section.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/mac_environment/scripts/functions.sh
-deleteAndAppendSection "#section" "/Users/$(echo $(logname))/parentnode/mac_environment/tests/delete_and_append_section_test_files/source" "/Users/$(echo $(logname))/Sites/parentnode/mac_environment/tests/delete_and_append_section_test_files/destination"
+deleteAndAppendSection "#section" "/Users/$(echo $(logname))/Sites/parentnode/mac_environment/tests/delete_and_append_section_test_files/source" "/Users/$(echo $(logname))/Sites/parentnode/mac_environment/tests/delete_and_append_section_test_files/destination"
