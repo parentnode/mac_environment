4fa9e52613abde9234667429ccb6c4b9b4db44b3
David Jessen <jessen-it@outlook.com>
2020-07-03 19:41:11 +0200

    Testing changes to mac script

---
+++ scripts/new_user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+source /srv/tools/scripts/functions.sh
+
+outputHandler "section" "Setting up parentnode webstack for a new mac user"
+
+cp /srv/tools/conf/dot_profile /home/$(logname)/.bash_profile
+outputHandler "comment" "exit and open the terminal to setup the final steps of your current user"
